9757b9bd2fa74d23d947b5491b38b5c5714c3dcd
AZWOLD <faycalkdj0@gmail.com>
2025-05-27 18:58:11 +0100

    Cleaned Code

--- BitCoin_AI.py
+++ BitCoin_AI.py
@@ -1,12 +1,14 @@
+#Libraries:
 import requests
 import pandas as pd
+#=============Variable/Configurators=================
 days = 89
 url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
 params = {
   "vs_currency" : "usd",
   "days" : days
 }
-
+#=============Fetching/Requesting DATA + Formating===============
 response = requests.get(url,params=params)
 data = response.json()
 DF = pd.DataFrame(data)
