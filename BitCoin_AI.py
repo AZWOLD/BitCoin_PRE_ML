Fine Tuned the model slightly and compared results/fixed Scaling problem and data leakage

BitCoin_AI.py
@@ -9,7 +9,7 @@ from keras.layers import LSTM, Dense
 ML_model = Sequential()
 #===================Variables/Configurators======================
 days = 89
-window_size=60
+window_size=72
 url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
 params = {
   "vs_currency" : "usd",
@@ -32,18 +32,19 @@ response = requests.get(
 data = response.json()
 DF = pd.DataFrame(data)
 #======================Parsing Prices============================
-prices = Scaler.fit_transform(np.array([i[1] for i in data["prices"]]).reshape(-1,1))
-X,y = Creat_Seq(prices,window_size)
-split = int(len(X) * 0.8)
-X_train,X_test = X[:split],X[split:]
-y_train,y_test = y[:split],y[split:]
+prices = np.array([i[1] for i in data["prices"]]).reshape(-1,1)
+split = int(len(prices) * 0.8)
+prices_TR = Scaler.fit_transform(prices[:split])
+prices_TS = Scaler.transform(prices[split:])
+X_train,y_train = Creat_Seq(prices_TR,window_size)
+X_test,y_test = Creat_Seq(prices_TS,window_size)
 
 Time_index = [i[0] for i in data["prices"]]
 DF.index = Time_index
-ML_model.add(LSTM(50,return_sequences=False,input_shape=(X_train.shape[1],X_train.shape[2])))
+ML_model.add(LSTM(55,return_sequences=False,input_shape=(X_train.shape[1],X_train.shape[2])))
 ML_model.add(Dense(1))
 ML_model.compile(optimizer='adam',loss='mean_squared_error')
 ML_model.fit(X_train,y_train,epochs=30,batch_size=32,validation_split=0.1)
-
-Prices_Pred = ML_model.predict(X_test)
-print(Prices_Pred)
+np.set_printoptions(suppress=True, precision=4)
+Prices_Pred = Scaler.inverse_transform(ML_model.predict(X_test))
+y_test = Scaler.inverse_transform(y_test)